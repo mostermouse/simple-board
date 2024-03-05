a90059a66c3d1a313637224c796ae69727dbe830
mostermouse <whdls7946@gmail.com>
2024-03-05 09:45:46 +0900

    掲示板プロジェクト

---
+++ src/main/java/com/jpa/simpleboard/reply/service/ReplyService.java
@@ -0,0 +1,31 @@
+package com.jpa.simpleboard.reply.service;
+
+import com.jpa.simpleboard.reply.db.ReplyEntity;
+import com.jpa.simpleboard.reply.db.ReplyRepository;
+import com.jpa.simpleboard.reply.model.ReplyRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+@RequiredArgsConstructor
+public class ReplyService {
+    private final ReplyRepository replyRepository;
+
+    public ReplyEntity create(
+           ReplyRequest replyRequest
+    ){
+        var entity = ReplyEntity.builder()
+                .postId(replyRequest.getPostId())
+                .userName(replyRequest.getUserName())
+                .password(replyRequest.getPassword())
+                .status("REGISTERED")
+                .title(replyRequest.getTitle())
+                .content(replyRequest.getContent())
+                .repliedAt(LocalDateTime.now())
+                .build()
+                ;
+        return replyRepository.save(entity);
+    }
+}
